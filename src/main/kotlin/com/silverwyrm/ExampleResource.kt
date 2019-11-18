b70a80e506088017a3506a5cc401e3e50c19deb7
Erik Mayrhofer <erik.mayrhofer@liwest.at>
2019-11-18 13:31:26 +0100

    Made ExampleResource private

--- src/main/kotlin/com/silverwyrm/ExampleResource.kt
+++ src/main/kotlin/com/silverwyrm/ExampleResource.kt
@@ -1,14 +1,26 @@
 package com.silverwyrm
 
+import io.quarkus.security.Authenticated
+import io.quarkus.security.identity.SecurityIdentity
+import io.smallrye.jwt.auth.principal.JWTCallerPrincipal
+import javax.inject.Inject
 import javax.ws.rs.GET
 import javax.ws.rs.Path
 import javax.ws.rs.Produces
 import javax.ws.rs.core.MediaType
+import javax.ws.rs.core.Response
 
 @Path("/hello")
+@Authenticated
 class ExampleResource {
 
+    @Inject
+    private lateinit var identity: SecurityIdentity
+
     @GET
-    @Produces(MediaType.TEXT_PLAIN)
-    fun hello() = "hello"
+    @Produces(MediaType.APPLICATION_JSON)
+    fun hello(): Response {
+        val principal = identity.principal as JWTCallerPrincipal
+        return Response.ok(principal).build()
+    }
 }
